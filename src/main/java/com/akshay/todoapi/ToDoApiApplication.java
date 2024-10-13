4da20938a2631cf80020187718576016335179df
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2024-10-13 13:20:32 +0530

    initialising project

---
+++ src/main/java/com/akshay/todoapi/ToDoApiApplication.java
@@ -0,0 +1,12 @@
+package com.akshay.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoApiApplication.class, args);
+    }
+}
---
+++ src/main/java/com/akshay/todoapi/ToDoController.java
@@ -0,0 +1,4 @@
+package com.akshay.todoapi;
+
+public class ToDoController {
+}
