0c0d5ecf504688b95418482691dd14a22868fbe9
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2024-10-13 14:38:25 +0530

    added status code for post request

--- src/main/java/com/akshay/todoapi/ToDoController.java
+++ src/main/java/com/akshay/todoapi/ToDoController.java
@@ -1,9 +1,7 @@
 package com.akshay.todoapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +23,7 @@ public class ToDoController {
     }
 
     @PostMapping("/todos")
+    @ResponseStatus(HttpStatus.CREATED)
     public Todo createTodo(@RequestBody Todo newTodo) {
         todoList.add(newTodo);
         return newTodo;
