0941e0060eab51cc93e0f1f82d8e40662e7fecea
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2024-10-13 20:30:31 +0530

    changed get all todos on basis of query params

--- src/main/java/com/akshay/todoapi/ToDoController.java
+++ src/main/java/com/akshay/todoapi/ToDoController.java
@@ -21,8 +21,18 @@ public class ToDoController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Todo>> getTodos() {
-        return ResponseEntity.status(HttpStatus.OK).body(todoList);
+    public ResponseEntity<List<Todo>> getTodos(@RequestParam(required = false) Boolean isCompleted) {
+        if(isCompleted == null) {
+            return ResponseEntity.status(HttpStatus.OK).body(todoList);
+        }
+
+        List<Todo> filteredTodos = new ArrayList<>();
+        for (Todo todo: todoList) {
+            if(todo.isCompleted() == isCompleted) {
+                filteredTodos.add(todo);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(filteredTodos);
     }
 
     @PostMapping()
