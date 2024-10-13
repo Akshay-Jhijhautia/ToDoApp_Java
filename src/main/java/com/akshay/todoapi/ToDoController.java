c41979431512d71f0526bce2a8e1750e72119748
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2024-10-13 13:55:18 +0530

    created basic controller structure

--- src/main/java/com/akshay/todoapi/ToDoController.java
+++ src/main/java/com/akshay/todoapi/ToDoController.java
@@ -1,4 +1,18 @@
 package com.akshay.todoapi;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class ToDoController {
+
+    private static List<Todo> todos;
+
+    public ToDoController() {
+        todos = new ArrayList<>();
+        todos.add(new Todo(1,false,"Todo 1",1));
+        todos.add(new Todo(2,true,"Todo 2",2));
+    }
 }
